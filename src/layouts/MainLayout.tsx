4a4133d73a6f737e17d43910933274a7a3cf8b4a
Edgar <eadrian2018@gmail.com>
2026-07-28 12:58:54 -0700

    Add AI Dental Assistant UI and complete website layout refinements

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import TopBar from "../components/TopBar/TopBar";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import WhatsAppButton from "../components/WhatsAppButton/WhatsAppButton";
+
 import BackToTop from "../components/BackToTop/BackToTop";
 import ScrollToTop from "../components/ScrollToTop";
 
@@ -27,7 +27,7 @@ export default function MainLayout() {
 
       <Footer />
 
-      <WhatsAppButton />
+     {/* <WhatsAppButton /> */}
 
       <BackToTop />
 
